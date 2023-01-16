f0a6839380347185c5e0142058abe814fd2d7f74
Lev Averin <lev.averin@telekom.com>
2023-01-16 17:01:40 +0200

    Initial commit

---
+++ src/main/java/com/example/pastestorage/PastestorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.pastestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastestorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastestorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pastestorage/PastestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pastestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastestorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
