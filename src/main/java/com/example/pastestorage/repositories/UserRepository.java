792fc77eb48ef072dc67ca0829e4a49fbef426cf
Lev Averin <lev.averin@telekom.com>
2023-02-14 07:45:03 +0200

    Added JPQL query to user repository in order to make Hibernate filters work

--- src/main/java/com/example/pastestorage/repositories/UserRepository.java
+++ src/main/java/com/example/pastestorage/repositories/UserRepository.java
@@ -2,9 +2,12 @@ package com.example.pastestorage.repositories;
 
 import com.example.pastestorage.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    Optional<User> findByUsername(@Param("username") String username);
 }
