9ecbfd834d85270a84fdb229fac3693967e2a20d
Lev Averin <lev.averin@telekom.com>
2023-02-20 10:30:07 +0200

    Added setter

--- src/main/java/com/example/pastestorage/dto/request/SetUserRoleDTO.java
+++ src/main/java/com/example/pastestorage/dto/request/SetUserRoleDTO.java
@@ -3,8 +3,10 @@ package com.example.pastestorage.dto.request;
 import com.example.pastestorage.dto.BaseDTO;
 import com.example.pastestorage.types.UserRole;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class SetUserRoleDTO extends BaseDTO {
     UserRole role;
 }
